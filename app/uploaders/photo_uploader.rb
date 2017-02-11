e975062576f8142aa4e7f20f4a6a63f8c1ddd7ec
Carl de REYNAL <carldereynal@gmail.com>
2017-02-11 15:44:10 +0100

    style improved all over the app

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,14 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+  process eager: true
+
+  process convert: 'jpg'
+
+  version :standard do
+    resize_to_fit 600, 400
+  end
+
+  version :card do
+    resize_to_fit 400, 300
+  end
 end
